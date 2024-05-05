ace634bb279952a702d34542c47118adab00df44
Milan Lakhani <milan.l@hotmail.co.uk>
2024-05-05 10:13:09 +0100

    Should work

--- models.py
+++ models.py
@@ -59,80 +59,80 @@ class BasicBlock(nn.Module):
 
         return out
 
-# class ResNet(nn.Module):
-#     def __init__(self, block, layers, num_classes):
-#         super(ResNet, self).__init__()
-#         self.in_channels = 64
-#         self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
-#         self.bn1 = nn.BatchNorm2d(64)
-#         self.relu = nn.ReLU(inplace=True)
-#         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-#         self.layer1 = self._make_layer(block, 64, layers[0])
-#         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-#         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-#         self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-#         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-#         self.fc = nn.Linear(512 * block.expansion, num_classes)
+class ResNet(nn.Module):
+    def __init__(self, block, layers, num_classes):
+        super(ResNet, self).__init__()
+        self.in_channels = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
 
 # # Waste Classifier Model
-# class WasteClassifier(nn.Module):
-#     def __init__(self, num_classes):
-#         super(WasteClassifier, self).__init__()
-#         self.features = nn.Sequential(
-#             nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
-#             nn.ReLU(),
-#             nn.MaxPool2d(kernel_size=2, stride=2),
-#             nn.Conv2d(32, 64, kernel_size=3, padding=1),
-#             nn.ReLU(),
-#             nn.MaxPool2d(kernel_size=2, stride=2)
-#         )
-#         self.classifier = nn.Sequential(
-#             nn.Dropout(0.5),
-#             nn.Linear(64 * 56 * 56, 1024),
-#             nn.ReLU(),
-#             nn.Linear(1024, num_classes)
-#         )
+class WasteClassifier(nn.Module):
+    def __init__(self, num_classes):
+        super(WasteClassifier, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(64 * 56 * 56, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, num_classes)
+        )
 
-#     def new_method(self, num_classes):
-#         return num_classes
+    def new_method(self, num_classes):
+        return num_classes
 
-#     def forward(self, x):
-#         x = self.features(x)
-#         x = torch.flatten(x, 1)
-#         x = self.classifier(x)
-#         return x
+    def forward(self, x):
+        x = self.features(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
 
-#     def _make_layer(self, block, out_channels, blocks, stride=1):
-#         downsample = None
-#         if stride != 1 or self.in_channels != out_channels * block.expansion:
-#             downsample = nn.Sequential(
-#                 nn.Conv2d(self.in_channels, out_channels * block.expansion, kernel_size=1, stride=stride, bias=False),
-#                 nn.BatchNorm2d(out_channels * block.expansion)
-#             )
+    def _make_layer(self, block, out_channels, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.in_channels != out_channels * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_channels, out_channels * block.expansion, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels * block.expansion)
+            )
 
-#         layers = [block(self.in_channels, out_channels, stride, downsample)]
-#         self.in_channels = out_channels * block.expansion
-#         for _ in range(1, blocks):
-#             layers.append(block(self.in_channels, out_channels))
+        layers = [block(self.in_channels, out_channels, stride, downsample)]
+        self.in_channels = out_channels * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.in_channels, out_channels))
 
-#         return nn.Sequential(*layers)
+        return nn.Sequential(*layers)
 
-#     def forward(self, x):
-#         x = self.conv1(x)
-#         x = self.bn1(x)
-#         x = self.relu(x)
-#         x = self.maxpool(x)
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
 
-#         x = self.layer1(x)
-#         x = self.layer2(x)
-#         x = self.layer3(x)
-#         x = self.layer4(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
 
-#         x = self.avgpool(x)
-#         x = torch.flatten(x, 1)
-#         x = self.fc(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
 
-#         return x
+        return x
 
 import torch
 import torch.nn as nn
